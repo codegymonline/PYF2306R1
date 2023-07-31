058351ceca488ea4d1a82edebe55e0d6e7bcb44c
Hien Luong <hyhien@gmail.com>
2023-07-31 21:33:05 +0700

    Resource day 23

---
+++ Buoi23_Tkinter/Demo01.py
@@ -0,0 +1,14 @@
+# 1. Chèn thư viện
+from tkinter import *
+
+# 2. Khai báo cửa sổ chính
+main = Tk()
+
+# 2.x Tinh chỉnh cửa số chính
+main.title("Cửa số chính")
+main.geometry("300x200")
+
+# 3. Khai báo và nhúng các control (widget) lên cửa sổ chính
+
+# 4. Chạy cửa sổ chính
+main.mainloop()
